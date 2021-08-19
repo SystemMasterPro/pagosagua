3b5103d5d3d2e2635cdc4e3afc58c8b387213d56
lidia1923 <62458634+lidia1923@users.noreply.github.com>
2021-08-18 21:26:08 -0500

    Update wsgi.py

--- proyectoTesis/wsgi.py
+++ proyectoTesis/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoTesis.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoTesis.settings')
 
 #application = get_wsgi_application()
 
