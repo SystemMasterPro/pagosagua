df3f244e59eeef7b33e005aa741587f6c0a2ba63
lidia1923 <62458634+lidia1923@users.noreply.github.com>
2021-08-18 21:27:09 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoTesis.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoTesis.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
